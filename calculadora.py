71c480c85d798b1a6aef492f1bee7448d8263bb3
Daiane Carvalho <dayenika213@gmail.com>
2026-08-31 15:27:31 -0300

    Primeiro commit - calculadora em Python

---
+++ calculadora.py
@@ -0,0 +1,16 @@
+#calculadora
+num1 = int(input("Digite o primeiro numero: "))
+num2 = int(input("Digite o segundo numero: "))
+operacao = input("Digite a operação: ")
+
+match operacao:
+    case "+":
+        res = num1 + num2
+    case "-":
+        res = num1 - num2
+    case "*":
+        res = num1 * num2
+    case "/":
+        res = num1 / num2
+
+print(f"Resultado é igual a {res}")
